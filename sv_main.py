e548d6b3f8cdb8e7e3cb5056c8bac299037bfb38
cz2014 <zcmben2014@gmail.com>
2019-10-12 20:26:45 -0500

    nothing special

--- sv_main.py
+++ sv_main.py
@@ -9,14 +9,16 @@ eflist5 = [[0,0],[1e5,0],[3e5,0],[6e5,0],[10e5,0],[15e5,0],[2e6,0],[3e6,0],[4e6,
 eflist6 = [[0,0],[3e5,0],[6e5,0],[15e5,0],[3e6,0],[7e6,0],[13e6,0],[20e6,0],[30e6,0]]
 eflist7 = [[30e6,0]]
 eflist8 = [[0,0],[3e5,0],[5e5,0],[7e5,0],[9e5,0],[11e5,0],[13e5,0],[15e5,0]]
+eflist10 = [[0,0],[1e4,0],[2e4,0],[3e4,0],[4e4,0],[5e4,0],[6e4,0],[7e4,0],[8e4,0],[9e4,0],[10e4,0]]
+
 
 
 sv_test = sv.MCseries(
     NK=120, 
     NBND=1, 
     NBNDv=4, 
-    reci_vec=[[0.314027,  0.181303],[0.000000,  0.362607]], 
-    prefix='./MoSe2_tri',
+    reci_vec=np.array([[0.313724, 0.181128],[0.000000,  0.362257]]), 
+    prefix='./WS2',
     test_mode=2,
     # governed by test_mode
     # stat_e=1,
@@ -24,7 +26,8 @@ sv_test = sv.MCseries(
     # stat_v=1,
     # end
     nelec=1,
-    ds_file='ds90_1bnd_cut1e-8',
+    ds_file='ds',
+    # ds_file='ds180_4bnd_cut1e-8',
     wan_file='tt_geninterp.dat',
     scf_file='scf120.out',
     Wan_Band_List=[0],
@@ -36,7 +39,8 @@ sv_test = sv.MCseries(
     totstep=10000,
     # output='sv.out',
     # enable_para=True,
-    # Nprocess=4
+    # Nprocess=4,
+    iverbosity=1
     )
 
 if __name__ == '__main__':
